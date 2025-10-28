d6ed0e2f6f4bbd81f46fcadad6ed0735d1a04b72
Muhammad Abdullah Azzam <mabdullahazzam394@gmail.com>
2025-10-28 19:22:10 +0700

    Upload_create_transactions_table

---
+++ database/migrations/2025_10_28_115300_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('transactions_group_id', 100);
+            $table->integer('user_account_id')->unique('user_account_id');
+            $table->integer('financial_account_id')->unique('financial_account_id');
+            $table->enum('entry_type', ['debit', 'credit']);
+            $table->bigInteger('amount');
+            $table->enum('balance_effect', ['increase', 'decrease']);
+            $table->integer('description');
+            $table->boolean('is_balance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
