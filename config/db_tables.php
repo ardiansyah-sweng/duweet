10da07169e0f677211deb62c83c670a1671dbfb9
Muhammad Abdullah Azzam <mabdullahazzam394@gmail.com>
2025-10-31 13:30:53 +0700

    Fix typo in user_account table name

--- config/db_tables.php
+++ config/db_tables.php
@@ -1,6 +1,7 @@
 <?php
 
     return [
+        'user_account'        => 'user_accounts',
         'financial_account'   => 'financial_accounts',
-        'transaction'   => 'transactions'
+        'transaction'         => 'transactions',
     ];
