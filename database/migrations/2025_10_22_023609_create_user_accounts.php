047d787b9ea556cbae995a5ce9635a4f6d008022
Aydria196 <muhaydriazulfa@gmail.com>
2025-11-01 21:20:38 +0700

    revisi tabel UserAccount di database/migrations

--- database/migrations/2025_10_22_023609_create_user_accounts.php
+++ database/migrations/2025_10_22_023609_create_user_accounts.php
@@ -3,27 +3,32 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Constants\UserAccountColumns;
 
 return new class extends Migration
 {
+    protected string $table;
+
+    public function __construct()
+    {
+        $this->table = config('db_tables.user_account');
+    }
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('user_accounts', function (Blueprint $table) {
-            $table->bigIncrements('id_userAccount'); 
-            $table->foreignId('id_user')
-                  ->constrained('users') 
-                  ->onDelete('cascade'); 
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->id(); 
+            $table->unsignedBigInteger(UserAccountColumns::ID_USER); 
+            $table->string(UserAccountColumns::USERNAME)->unique();
+            $table->string(UserAccountColumns::EMAIL)->unique();
+            $table->string(UserAccountColumns::PASSWORD);
+            $table->timestamp(UserAccountColumns::VERIFIED_AT)->nullable();
+            $table->boolean(UserAccountColumns::IS_ACTIVE)->default(true);           
+            $table->timestamps();
 
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('status')->default('active');           
-            $table->timestamp('tanggal_daftar')->nullable()->useCurrent();
-            $table->timestamp('tanggal_update')->nullable();
-            //softdeletes untuk menghapus
-            $table->softDeletes('tanggal_hapus'); 
+            $table->foreign(UserAccountColumns::ID_USER)->references(UserAccountColumns::ID_USER)->on($this->table)->onDelete('cascade');
         });
     }
 
@@ -33,6 +38,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_accounts');
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists($this->table);
+        Schema::enableForeignKeyConstraints();
     }
 };
