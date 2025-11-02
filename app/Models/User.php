1f79e9f4797899d7055c784e1f4d656efeb2f8fd
Alya0507 <alyazhafiraniha@gmail.com>
2025-11-02 11:11:43 +0700

    query update

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'photo',      
+        'preference',
     ];
 
     /**
@@ -43,6 +45,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'preference' => 'array', //tambahan preferences
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // Tambahkan ini
 
+// Route bawaan Laravel (halaman utama)
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk update data user
+Route::middleware('auth')->put('/user/update', [UserController::class, 'update'])->name('user.update');
