365c6c6ae895d14d21d9d941b0dba9f65c32cb39
Delwyn <delwynputradaren@gmail.com>
2025-11-07 09:11:41 +0700

    update

---
+++ app/Constants/AccountColumns.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Constants;
+
+class AccountColumns
+{
+    public const ID               = 'id';
+    public const PARENT_ID        = 'parent_id';
+    public const NAME             = 'name';
+    public const TYPE             = 'type';
+    public const BALANCE          = 'balance';
+    public const INITIAL_BALANCE  = 'initial_balance';
+    public const IS_GROUP         = 'is_group';
+    public const DESCRIPTION      = 'description';
+    public const IS_ACTIVE        = 'is_active';
+    public const SORT_ORDER       = 'sort_order';
+    public const LEVEL            = 'level';
+}
