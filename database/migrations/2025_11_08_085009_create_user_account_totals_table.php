2a7207823ddf972b5dbbd109602badac32124c50
aldi205 <aldimusifa20@gmail.com>
2025-11-08 16:16:52 +0700

    2400018205 query hitung total account per user

---
+++ database/migrations/2025_11_08_085009_create_user_account_totals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('user_account_totals', function (Blueprint $table) {
+            $table->id();
+
+            // Relasi ke user dan akun
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('account_id');
+
+            // Total saldo akun per user
+            $table->decimal('total_balance', 15, 2)->default(0);
+            $table->decimal('initial_balance', 15, 2)->default(0);
+
+            $table->timestamps();
+
+            // Foreign key
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('user_account_totals');
+    }
+};
