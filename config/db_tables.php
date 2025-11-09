052abf60d6b64088a47584f4283704e54d36d0d5
MuhammadHaidarPutra <2400018209@webmail.uad.ac.id>
2025-11-10 06:27:29 +0700

    Normalize spacing in config/db_tables.php

--- config/db_tables.php
+++ config/db_tables.php
@@ -5,6 +5,6 @@ return [
     'user_account' => 'user_accounts',
     'financial_account' => 'financial_accounts',
     'user_financial_account' => 'user_financial_accounts',
-    'transaction'=> 'transactions'
+    'transaction' => 'transactions'
      
 ];
