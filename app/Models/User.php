3e6e639db3c69deda0060ec66725a739962039d0
intanoktaavv <2400018195@webmail.uad.ac.id>
2025-11-10 12:35:17 +0700

    getDataLogin  and update fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,59 +9,46 @@ use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        public static function getDataLogin($accountId)
-{
-    return DB::table('users AS u')
-        ->join('user_accounts AS ua', 'ua.user_id', '=', 'u.id')
-        ->leftJoin('user_telephones AS ut', 'ut.user_id', '=', 'u.id')
-        ->select(
-            'u.id AS user_id',
-            'u.name',
-            'u.first_name',
-            'u.middle_name',
-            'u.last_name',
-            'u.email AS user_email',
-            'u.tanggal_lahir',
-            'u.bulan_lahir',
-            'u.tahun_lahir',
-            'u.usia',
-            'ut.number AS telephone_number',
-            'ua.id AS account_id',
-            'ua.username',
-            'ua.email AS login_email',
-            'ua.is_active',
-            'ua.email_verified_at'
-        )
-        ->where('ua.id', $accountId)
-        ->first();
-}
-
+        'name',
+        'email',
+        'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    public static function getDataLogin(int $accountId): ?object
+    {
+        return DB::table('users AS u')
+            ->join('user_accounts AS ua', 'ua.user_id', '=', 'u.id')
+            ->leftJoin('user_telephones AS ut', 'ut.user_id', '=', 'u.id')
+            ->select(
+                'u.id AS user_id',
+                'u.name',
+                'u.first_name',
+                'u.middle_name',
+                'u.last_name',
+                'u.email AS user_email',
+                'u.tanggal_lahir',
+                'u.bulan_lahir',
+                'u.tahun_lahir',
+                'u.usia',
+                'ut.number AS telephone_number',
+                'ua.id AS account_id',
+                'ua.username',
+                'ua.email AS login_email',
+                'ua.is_active',
+                'ua.email_verified_at'
+            )
+            ->where('ua.id', $accountId)
+            ->first();
+    }
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -71,4 +57,3 @@ class User extends Authenticatable
         ];
     }
 }
-
