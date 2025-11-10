c880f1c1f3d0fbbcf7bca203eab07a8e4d1c943f
Alya0507 <alyazhafiraniha@gmail.com>
2025-11-10 12:37:07 +0700

    Revisi menambahkan perintah query update
    Added editUser method to facilitate user updates.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB; // tambahkan untuk query builder
 
 class User extends Authenticatable
 {
@@ -21,8 +22,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'photo',      
-        'preference',
+        'photo', //tambahan photo
+        'preference', //tambahan preference
     ];
 
     /**
@@ -45,7 +46,23 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'preference' => 'array', //tambahan preferences
+            'preference' => 'array',
         ];
+    }
+
+    /**
+     * Query perintah edit user
+     */
+    public static function editUser($id, $data)
+    {
+        return DB::table('users')
+            ->where('id', $id)
+            ->update([
+                'name' => $data['name'] ?? null,
+                'email' => $data['email'] ?? null,
+                'photo' => $data['photo'] ?? null,
+                'preference' => isset($data['preference']) ? json_encode($data['preference']) : null,
+                'updated_at' => now(),
+            ]);
     }
 }
