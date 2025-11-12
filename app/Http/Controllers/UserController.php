c208359c2f0b2d7fbec8bf154371402b7c079db0
man <2400018185@webmail.uad.ac.id>
2025-11-12 16:40:55 +0700

    update lagi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use App\Constants\UserColumns;
 use App\Constants\UserAccountColumns;
 
 class UserController extends Controller
 {
+    /**
+     * Ambil semua account yang dimiliki user via UserAccount
+     * Output JSON dengan kolom ID User, Username, Email, Verified, Is Active
+     */
     public function getAllAccounts($id)
     {
-        $user = User::find($id);
+        // Ambil user langsung dari tabel menggunakan db_tables.php
+        $userTable = config('db_tables.user');
+        $accountTable = config('db_tables.user_account');
+
+        $user = DB::table($userTable)
+            ->where(UserColumns::ID, $id)
+            ->first();
 
         if (!$user) {
             return response()->json([
@@ -17,8 +28,12 @@ class UserController extends Controller
             ], 404);
         }
 
-        $accounts = $user->userAccounts()->get();
+        // Ambil semua akun user dari tabel user_accounts
+        $accounts = DB::table($accountTable)
+            ->where(UserAccountColumns::ID_USER, $id)
+            ->get();
 
+        // Map data ke format JSON yang rapi
         $data = $accounts->map(function($account) {
             return [
                 'ID User'   => $account->{UserAccountColumns::ID_USER},
@@ -29,9 +44,10 @@ class UserController extends Controller
             ];
         });
 
+        // Kembalikan JSON
         return response()->json([
-            'User ID' => $user->id,
-            'Name'    => $user->name,
+            'User ID' => $user->{UserColumns::ID},
+            'Name'    => $user->{UserColumns::NAME},
             'Accounts'=> $data
         ], 200);
     }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\UserController;
 
 Route::get('/user/{id}/accounts', [UserController::class, 'getAllAccounts']);
