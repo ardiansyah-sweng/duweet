29671e10f5fbca7d467849e74ce9850e12f37d12
Khansafarras <2400018190@webmail.uad.ac.id>
2025-11-12 12:17:53 +0700

    revisi user php merapikan blok program

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,42 +2,26 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB; 
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -45,23 +29,24 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    public static function getAllUsersRaw()
-{
-    $query = "
-        SELECT
-            id,
-            CONCAT_WS(' ', first_name, middle_name, last_name) as full_name,
-            email,
-            nomor_telepon,
-            role,
-            is_active,
-            created_at
-        FROM
-            users
-        ORDER BY
-            first_name ASC
-    ";
 
-    return DB::select($query);
-}
+    public static function getAllUsersRaw()
+    {
+        $query = "
+            SELECT
+                id,
+                CONCAT_WS(' ', first_name, middle_name, last_name) AS full_name,
+                email,
+                nomor_telepon,
+                role,
+                is_active,
+                created_at
+            FROM
+                users
+            ORDER BY
+                first_name ASC
+        ";
+
+        return DB::select($query);
+    }
 }
