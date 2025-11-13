24bfd9a6bfca6f5f18038a70856b9c1d75619baa
RafiSatyaPrayoga <rafisatyaprayoga@gmail.com>
2025-11-13 13:03:50 +0700

    perubahan agar tidak conflict

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\ReportController;
 
+Route::prefix('user-account')->group(function () {
+    Route::get('/', [UserAccountController::class, 'index'])->name('api.user-account.index');
+    Route::get('/{id}', [UserAccountController::class, 'show'])->name('api.user-account.show');
+    Route::post('/', [UserAccountController::class, 'store'])->name('api.user-account.store');
+    Route::put('/{id}', [UserAccountController::class, 'update'])->name('api.user-account.update');
+    Route::delete('/{id}', [UserAccountController::class, 'destroy'])->name('api.user-account.destroy');
+    Route::delete('/{id}/raw', [UserAccountController::class, 'destroyRaw'])->name('api.user-account.destroy-raw');
+});
+
 Route::get('/ping', fn () => response()->json(['pong' => true]));
 
 Route::get('/accounts', function () {
