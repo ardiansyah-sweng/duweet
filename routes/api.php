31abb693221aea4b9662b16693c433625f7b5d47
Faridh237 <2400018237@webmail.uad.ac.id>
2025-11-15 18:37:05 +0700

    Query Pencarian FinancialAccount

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAccountController;
+use App\Http\Controllers\FinancialAccountController;
 
 // UserAccount API Routes (no CSRF protection needed)
 Route::prefix('user-account')->group(function () {
@@ -11,4 +12,11 @@ Route::prefix('user-account')->group(function () {
     Route::put('/{id}', [UserAccountController::class, 'update'])->name('api.user-account.update');
     Route::delete('/{id}', [UserAccountController::class, 'destroy'])->name('api.user-account.destroy');
     Route::delete('/{id}/raw', [UserAccountController::class, 'destroyRaw'])->name('api.user-account.destroy-raw');
+});
+
+// FinancialAccount API Routes
+Route::prefix('financial-account')->group(function () {
+    Route::get('/', [FinancialAccountController::class, 'index'])->name('api.financial-account.index');
+    Route::get('/{id}', [FinancialAccountController::class, 'show'])->name('api.financial-account.show');
+    // other endpoints (store/update/delete) can be added similarly
 });
