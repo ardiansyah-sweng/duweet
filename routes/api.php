ccec9572cafd2a078497a342a3d1bcf1bd5fb82e
rafi <rafiauliarachman48@gmail.com>
2025-12-02 19:34:57 +0700

    menambahkan route yang terhapus sebelumnya di routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,11 @@ Route::get('/expenses/monthly', [MonthlyExpenseController::class, 'monthly']);
 // tambahkan route UserAccount sesuai kebutuhan, contoh:
 Route::get('/user-accounts', [UserAccountController::class, 'index']);
 Route::get('/user-accounts/{id}', [UserAccountController::class, 'show']);
+Route::prefix('user-account')->group(function () {
+    Route::get('/', [UserAccountController::class, 'index'])->name('api.user-account.index');
+    Route::get('/{id}', [UserAccountController::class, 'show'])->name('api.user-account.show');
+    Route::post('/', [UserAccountController::class, 'store'])->name('api.user-account.store');
+    Route::put('/{id}', [UserAccountController::class, 'update'])->name('api.user-account.update');
+    Route::delete('/{id}', [UserAccountController::class, 'destroy'])->name('api.user-account.destroy');
+    Route::delete('/{id}/raw', [UserAccountController::class, 'destroyRaw'])->name('api.user-account.destroy-raw');
+});
