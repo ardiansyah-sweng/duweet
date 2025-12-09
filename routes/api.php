d275f338846228676a79cd1430a67453f50976ac
man <2400018185@webmail.uad.ac.id>
2025-12-09 14:30:10 +0700

    Perbaikan route API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserAccountController;
 
-Route::prefix('user')->group(function () {
-    Route::get('{id}/accounts', [UserController::class, 'getAllAccounts']);
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('api.users.destroy');
+Route::get('/users/{id}/accounts', [UserController::class, 'getAllAccounts'])->name('api.users.accounts');
+
+// UserAccount API Routes (no CSRF protection needed)
+Route::prefix('user-account')->group(function () {
+    Route::get('/', [UserAccountController::class, 'index'])->name('api.user-account.index');
+    Route::get('/{id}', [UserAccountController::class, 'show'])->name('api.user-account.show');
+    Route::post('/', [UserAccountController::class, 'store'])->name('api.user-account.store');
+    Route::put('/{id}', [UserAccountController::class, 'update'])->name('api.user-account.update');
+    Route::delete('/{id}', [UserAccountController::class, 'destroy'])->name('api.user-account.destroy');
+    Route::delete('/{id}/raw', [UserAccountController::class, 'destroyRaw'])->name('api.user-account.destroy-raw');
 });
