691e5b312fe2f3af6359a2d740b9f446f7e2044a
MahfutTri <mahfuttri8a@gmail.com>
2025-12-09 16:05:42 +0700

    resolve: bersihkan konflik dan gabungkan semua seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             UserAccountSeeder::class,
-<<<<<<< HEAD
             AccountSeeder::class,
             TransaksiSeeder::class,
-=======
-            //AccountSeeder::class,
->>>>>>> f69f6a334e79a0c91b6090aee470fb63b59926ce
         ]);
     }
 }
