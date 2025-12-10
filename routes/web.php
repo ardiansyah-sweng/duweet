66fdacf1ab545ca91f14404985820ec0ec64bb8c
Muhammad Aziz Fathirrahman <azizzfathirr@gmail.com>
2025-12-10 19:27:41 +0700

    "Update kesalahan menghapus yang asli pada web"

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,19 @@
 <?php
 
-use App\Http\Controllers\FinancialAccountController; 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\FinancialAccountController;
+
+// -------------------------------------------------------------
+// Web Routes
+// -------------------------------------------------------------
+
+// Route dasar bawaan Laravel
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route GET endpoint incomeSummary
+Route::get('/report/income-summary', [ReportController::class, 'incomeSummary']);
 
 Route::get('/financial-accounts/active', [FinancialAccountController::class, 'getActiveAccounts']);
