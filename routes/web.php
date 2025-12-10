7b07c7d88b36db44f0e1aa9a15208c9420a95526
Muhammad Aziz Fathirrahman <azizzfathirr@gmail.com>
2025-12-10 19:30:39 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\FinancialAccountController;
 
-// -------------------------------------------------------------
-// Web Routes
-// -------------------------------------------------------------
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-// Route dasar bawaan Laravel
+// Route dasar Laravel
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,4 +23,5 @@ Route::get('/', function () {
 // Route GET endpoint incomeSummary
 Route::get('/report/income-summary', [ReportController::class, 'incomeSummary']);
 
+// Route GET endpoint untuk Financial Accounts yang aktif
 Route::get('/financial-accounts/active', [FinancialAccountController::class, 'getActiveAccounts']);
