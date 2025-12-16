1a8ee2f9173d9114667c4520af3789c42c06d172
man <2400018185@webmail.uad.ac.id>
2025-12-16 15:40:06 +0700

    upgrade

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ use App\Http\Controllers\ReportController; // PENTING: Import Controller
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route GET yang Benar untuk endpoint incomeSummary
+// FIX: Menggantikan syntax lama dengan syntax array [Controller::class, 'method']
+Route::get('/report/income-summary', [ReportController::class, 'incomeSummary']);
+
