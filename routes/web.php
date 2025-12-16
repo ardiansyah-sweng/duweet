39ca7ff82d6edc70c3a9cb06d418c834f28cf1c3
Muhammad Aziz Fathirrahman <azizzfathirr@gmail.com>
2025-12-16 16:25:49 +0700

    fix: add web routes for FinancialAccount to support requests without /api prefix (helps Postman)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController; // PENTING: Import Controller
+use App\Http\Controllers\FinancialAccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,7 @@ Route::get('/', function () {
 Route::get('/report/income-summary', [ReportController::class, 'incomeSummary']);
 
 // Tambahkan route lain di sini jika ada...
+// Convenience web routes so hitting `/financial-account` in tools like Postman
+// (or browsers) will return the same JSON as the API endpoint.
+Route::get('/financial-account', [FinancialAccountController::class, 'index']);
+Route::get('/financial-account/{id}', [FinancialAccountController::class, 'show']);
