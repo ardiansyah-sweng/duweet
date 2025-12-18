d1ac54880ef7499bc5641a073aa0429d7f33e5f0
Nurrahmat Eko Nugroho <nurrahmat767@gmail.com>
2025-12-18 13:30:44 +0700

    Refactor FinancialAccountSeeder to disable foreign key checks before deleting existing data

--- database/seeders/FinancialAccountSeeder.php
+++ database/seeders/FinancialAccountSeeder.php
@@ -13,8 +13,10 @@ class FinancialAccountSeeder extends Seeder
     {
         $table = config('db_tables.financial_account', 'financial_accounts');
 
-        // Clear existing data (compatible with SQLite and MySQL)
-        DB::table($table)->truncate();
+        // Disable foreign key checks to allow delete
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table($table)->delete();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         DB::table($table)->insert([
             [
