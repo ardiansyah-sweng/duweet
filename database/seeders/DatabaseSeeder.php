d21bf10a1911d4ce275522f262013c9d55c24247
Muhammad Abyan <abyanmuhammad006@gmail.com>
2025-12-22 20:59:14 +0700

    update conflict

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,19 +15,12 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             UserAccountSeeder::class,
-<<<<<<< HEAD
             //AccountSeeder::class,
             //FinancialAccountSeeder::class,
             //TransactionSeeder::class,
             //FinancialAccountSeeder::class,
             //UserFinancialAccountSeeder::class,
             //TransactionSeeder::class,
-=======
-            AccountSeeder::class,
-            FinancialAccountSeeder::class,
-            UserFinancialAccountSeeder::class,
-            TransactionSeeder::class,
->>>>>>> f4526a8f92a989a5a26d0d4d84264f8649478a42
             //AccountSeeder::class,
         ]);
     }
