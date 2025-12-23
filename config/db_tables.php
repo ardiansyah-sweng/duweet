2186a288ba90b1af9c2cdf874ae26df53ddd0102
RafiSatyaPrayoga <rafisatyaprayoga@gmail.com>
2025-12-23 15:56:19 +0700

    perbaikan db_tables

--- config/db_tables.php
+++ config/db_tables.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'user' => 'users',
+    'user' => 'users', 
     'user_account' => 'user_accounts',
     'financial_account' => 'financial_accounts',
     'user_financial_account' => 'user_financial_accounts',
-    'transaction' => 'transactions',
+    'transaction' => 'transactions'
 
 ];
