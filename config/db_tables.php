0b7ad070123a15c931a7c02c27311cc65cea626b
RafiSatyaPrayoga <rafisatyaprayoga@gmail.com>
2025-12-23 15:51:40 +0700

    perbaikan format db_tables.php

--- config/db_tables.php
+++ config/db_tables.php
@@ -1,10 +1,10 @@
 <?php
 return [
-        'user' => 'users',
-        'user_account'   => 'user_accounts',
-        'financial_account' => 'financial_accounts',
-        'user_financial_account' => 'user_financial_accounts',
-        'transaction' => 'transactions',
-        
-    ];
+    'user' => 'users',
+    'user_account' => 'user_accounts',
+    'financial_account' => 'financial_accounts',
+    'user_financial_account' => 'user_financial_accounts',
+    'transaction' => 'transactions',
+
+];
 
