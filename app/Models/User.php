547285cbf49b23ceedeee55aee247ff7712592cb
yahyattaqy <yahyajack90@gmail.com>
2025-12-23 17:05:50 +0700

    Perpindahan ke UserFinancialAccount

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,6 @@ use App\Constants\UserAccountColumns;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -92,36 +91,5 @@ class User extends Authenticatable
             ->using(UserFinancialAccount::class)
             ->withPivot('balance', 'initial_balance', 'is_active')
             ->withTimestamps();
-    }
-
-    /**
-     * Sum balances of all users grouped by financial account type.
-     * Only include financial accounts that are leaf (is_group = false) and active.
-     * Returns associative array keyed by type (IN, EX, SP, LI, AS) with integer totals.
-     *
-     * @return array<string,int>
-     */
-    public static function sumAllUsersFinancialAccountsByType(): array
-    {
-        $types = ['IN', 'EX', 'SP', 'LI', 'AS'];
-
-        $sql = <<<'SQL'
-            SELECT fa.type, SUM(ufa.balance) AS total_balance
-            FROM user_financial_accounts ufa
-            JOIN financial_accounts fa ON ufa.financial_account_id = fa.id
-            WHERE fa.is_group = ?
-              AND fa.is_active = ?
-              AND ufa.is_active = ?
-            GROUP BY fa.type
-        SQL;
-
-        $rows = DB::select($sql, [0, 1, 1]);
-
-        $result = array_fill_keys($types, 0);
-        foreach ($rows as $r) {
-            $result[$r->type] = (int) $r->total_balance;
-        }
-
-        return $result;
     }
 }
