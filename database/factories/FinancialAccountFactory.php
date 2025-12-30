de69e9d896ef8933ac0f850c40cd12a8571b0c25
Dimas Pratama <2400018215@webmail.uad.ac.id>
2025-12-30 15:39:51 +0700

    Refactor FinancialAccountFactory to use AccountColumns constants and simplify account name selection

--- database/factories/FinancialAccountFactory.php
+++ database/factories/FinancialAccountFactory.php
@@ -2,44 +2,29 @@
 
 namespace Database\Factories;
 
-use App\Constants\FinancialAccountColumns;
 use App\Models\FinancialAccount;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Constants\FinancialAccountColumns as AccountColumns;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FinancialAccount>
- */
 class FinancialAccountFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var class-string<\Illuminate\Database\Eloquent\Model>
-     */
     protected $model = FinancialAccount::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
-        $accountNames = ['Cash', 'BCA', 'BRI', 'BNI', 'Mandiri', 'Dana', 'OVO', 'Gopay', 'ShopeePay', 'Tabungan', 'Reksa Dana', 'Deposito'];
-        $types = ['IN', 'EX', 'SP', 'LI', 'AS'];
+        $types = ['IN','EX','SP','LI','AS'];
 
         return [
-            FinancialAccountColumns::NAME => $this->faker->randomElement($accountNames),
-            FinancialAccountColumns::PARENT_ID => null,
-            FinancialAccountColumns::TYPE => $this->faker->randomElement($types),
-            FinancialAccountColumns::BALANCE => $this->faker->numberBetween(0, 5_000_000),
-            FinancialAccountColumns::INITIAL_BALANCE => $this->faker->numberBetween(0, 1_000_000),
-            FinancialAccountColumns::IS_GROUP => $this->faker->boolean(10),
-            FinancialAccountColumns::DESCRIPTION => $this->faker->optional()->sentence(3),
-            FinancialAccountColumns::IS_ACTIVE => true,
-            FinancialAccountColumns::IS_LIQUID => $this->faker->boolean(70),
-            FinancialAccountColumns::SORT_ORDER => $this->faker->numberBetween(0, 10),
-            FinancialAccountColumns::LEVEL => 0,
+            AccountColumns::NAME => $this->faker->randomElement(['Cash','BCA','BRI','Dana','OVO','Gopay','Tabungan','Reksa Dana']),
+            AccountColumns::PARENT_ID => null,
+            AccountColumns::TYPE => $this->faker->randomElement($types),
+            AccountColumns::BALANCE => $this->faker->numberBetween(0, 5_000_000),
+            AccountColumns::INITIAL_BALANCE => 0,
+            AccountColumns::IS_GROUP => $this->faker->boolean(10),
+            AccountColumns::DESCRIPTION => $this->faker->optional()->sentence(),
+            AccountColumns::IS_ACTIVE => true,
+            AccountColumns::SORT_ORDER => $this->faker->numberBetween(0, 10),
+            AccountColumns::LEVEL => 0,
         ];
     }
 }
