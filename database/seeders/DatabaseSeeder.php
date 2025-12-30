bb26164057f41f726737bfe29fb356e0b1ff0722
Muhammad Abyan <abyanmuhammad006@gmail.com>
2025-12-30 17:06:14 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -51,7 +51,7 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-         // Run seeders in order: users -> user_accounts -> financial_accounts -> transactions
+        // Run seeders in order: users -> user_accounts -> financial_accounts -> transactions
         $this->call([
             FinancialAccountSeeder::class,
             UserSeeder::class,
