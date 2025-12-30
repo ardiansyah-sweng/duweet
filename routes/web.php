b2b7bde83a361f2cb5d470239446b96b1aab0a71
MahfutTri <mahfuttri8a@gmail.com>
2025-12-30 17:16:18 +0700

    Keep all original code from main (red) while preserving new changes (green)

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MonthlyExpenseController;
+
+
+Route::get('/expenses/monthly', [MonthlyExpenseController::class, 'monthly']);
+
 use App\Http\Controllers\FinancialAccountController; 
-use App\Http\Controllers\ReportController;
+use App\Http\Controllers\ReportController; // PENTING: Import Controller
 
 Route::get('/test/liquid-assets', function () {
     try {
