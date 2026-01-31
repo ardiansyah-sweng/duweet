ecebcccb74bb5de63f1882c5cb26186c93658545
AdityoBimo <adityobimosukoco@gmail.com>
2026-01-31 14:55:59 +0700

    random akun untuk login

--- database/seeders/UserLoginSeeder.php
+++ database/seeders/UserLoginSeeder.php
@@ -13,19 +13,19 @@ class UserLoginSeeder extends Seeder
         DB::table('user_login')->insert([
             [
                 'user_account_id' => 1,
-                'last_login_at' => Carbon::now()->subDays(10),
+                'last_login_at' => Carbon::create(2026, 1, rand(1, 31)),
             ],
             [
                 'user_account_id' => 2,
-                'last_login_at' => null,
+                'last_login_at' => Carbon::create(2026, 1, rand(1, 31)),
             ],
             [
                 'user_account_id' => 3,
-                'last_login_at' => Carbon::now()->subDays(5),
+                'last_login_at' => null, // simulasi belum pernah login
             ],
             [
                 'user_account_id' => 4,
-                'last_login_at' => Carbon::now()->subDays(1),
+                'last_login_at' => Carbon::create(2026, 1, rand(1, 31)),
             ],
         ]);
     }
