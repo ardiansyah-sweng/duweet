497dd8d3e5a293b0a9a3451a51293a9f91c0acc5
naufal1214 <2400018226@webmail.uad.ac.id>
2026-02-05 11:49:03 +0700

    perbaikan output

--- app/Models/UserFinancialAccount.php
+++ app/Models/UserFinancialAccount.php
@@ -163,30 +163,34 @@ class UserFinancialAccount extends Model
      */
     public static function getGroupBalanceByAccountType($accountType = null)
     {
-        $accountTypeFilter = $accountType !== null ? " AND fa.type = '" . strtoupper($accountType) . "'" : '';
+          $bindings = [];
+        $accountTypeFilter = '';
+        if ($accountType !== null) {
+            $accountTypeFilter = " AND fa.type = ?";
+            $bindings[] = strtoupper($accountType);
+        }
 
         $sql = "
-            SELECT 
+            SELECT
                 ufa.user_account_id,
-                ufa.financial_account_id,
                 fa.type AS account_type,
                 SUM(ufa.initial_balance) AS total_initial_balance,
                 SUM(ufa.balance) AS total_balance
-            FROM 
+            FROM
                 user_financial_accounts ufa
-            INNER JOIN 
+            INNER JOIN
                 financial_accounts fa ON ufa.financial_account_id = fa.id
-            WHERE 
+            WHERE
                 fa.is_active = 1
                 AND ufa.is_active = 1
                 {$accountTypeFilter}
-            GROUP BY 
-                ufa.user_account_id, ufa.financial_account_id, fa.type
-            ORDER BY 
+            GROUP BY
+                ufa.user_account_id, fa.type
+            ORDER BY
                 fa.type, ufa.user_account_id
         ";
-
-        return DB::select($sql);
+     
+        return DB::select($sql, $bindings);
     }
 }
     
